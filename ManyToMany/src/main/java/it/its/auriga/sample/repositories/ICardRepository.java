60645d39e97ecd1d9311903db6394160525a3f12
B2f4 <95875422+B2f4@users.noreply.github.com>
2023-03-21 14:23:00 +0100

    Delete ManyToMany directory

--- ManyToMany/src/main/java/it/its/auriga/sample/repositories/ICardRepository.java
+++
@@ -1,9 +0,0 @@
-package it.its.auriga.sample.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.its.auriga.sample.models.Card;
-
-public interface ICardRepository extends JpaRepository<Card, Integer>{
-
-}
--- ManyToMany/src/main/java/it/its/auriga/sample/services/IStudenteCorsoService.java
+++
@@ -1,9 +0,0 @@
-package it.its.auriga.sample.services;
-import it.its.auriga.sample.dto.StudenteCorsoDTO;
-
-public interface IStudenteCorsoService {
-
-	public StudenteCorsoDTO save(StudenteCorsoDTO studenteCorsoDTO);
-	
-	
-}
